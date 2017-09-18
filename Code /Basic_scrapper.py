09888dcba5403468cf4e1e3dad9fe9f9aaf123e4
Pradhvan <Pradhvanbisht@gmail.com>
2017-09-19 00:29:52 +0530

    Added intital template code and doc

---
+++ Code /Basic_scrapper.py
@@ -0,0 +1,15 @@
+# Request library
+import requests
+
+
+# Using requests library to make a HTTP GET request
+# Their are various request method GET , PUT,DELETE
+
+response = requests.get("https://hasjob.co/")
+
+# Check the status code of the request made
+# print response.staus_code
+
+
+# Getting the extracted content
+print response.content
